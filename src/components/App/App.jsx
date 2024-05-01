0c2d6b6ebc017d74b7a121801d04dbd069dcac89
EProko <lokki9526@gmail.com>
2024-05-01 06:03:04 +0300

    edits

---
+++ src/components/App/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification";
+
+export default function App() {
+  const [response, setClicks] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("feedback");
+
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(response));
+  }, [response]);
+
+  const totalFeedback = response.good + response.neutral + response.bad;
+  const positiveFeedback = Math.round((response.good / totalFeedback) * 100);
+
+  const updateFeedback = (feedbackType) => {
+    setClicks({
+      ...response,
+      [feedbackType]: response[feedbackType] + 1,
+    });
+
+    feedbackType === "reset" &&
+      setClicks({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      });
+  };
+
+  return (
+    <>
+      <Description />
+      <Options onUpdate={updateFeedback} total={totalFeedback} />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback
+          value={response}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      )}
+    </>
+  );
+}
